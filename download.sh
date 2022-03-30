7b92a5763767a78d505b236e690a782f5557e4e3
htuananh <anhnguyenhuutuan@vccorp.vn>
2022-03-30 17:57:14 +0700

    Fix typo

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for video_link in `cat link.txt`;
 do
-        yt-dl $video_link -f mp4 --progress -o "Courses/%(playlist_title)s/%(title)s.%(ext)s";
+        yt-dlp $video_link -f mp4 --progress -o "Courses/%(playlist_title)s/%(title)s.%(ext)s";
 done
